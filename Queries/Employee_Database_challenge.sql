d0a58aa7016ad15753edbc329e167e2224174427
Ian Dixon <iantdixon@gmail.com>
2022-08-02 12:38:20 -0400

    added more figures.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -18,9 +18,9 @@ ORDER BY e.emp_no;
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
-rt.title
-
-INTO unique_titles
+rt.title,
+rt.to_date
+--INTO unique_titles
 FROM retirement_titles as rt
 WHERE to_date = '9999-01-01'
 ORDER BY rt.emp_no, rt.to_date DESC;
@@ -32,4 +32,29 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+-- Get employees eligible for mentorship
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+-- INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+    ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Get mentorship titles count
+SELECT COUNT(me.title), me.title
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY count DESC;
+
 
